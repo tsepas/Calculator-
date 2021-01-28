3c80eae78971fdcd19c58bd003526dfcdf529c8e
Thanasis <athanasios.tsepas@gmail.com>
2021-01-28 08:51:01 +0200

    Add option 5 for square root

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,5 @@
+import math
+
 print (30 * '-')
 print('    C A L C U L A T O R')
 print (30 * '-')
@@ -7,12 +9,16 @@ print ("1. Addition +")
 print ("2. Subtraction -")
 print ("3. Proliferation *")
 print ("4. Division /")
+print ("5. Square root √")
 print (30 * '-')
 
-choice=int(input(' Enter your choice [1-4] : '))
+choice=int(input(' Enter your choice [1-5] : '))
 
-number1=float(input('Enter your first number : '))
-number2=float(input('Enter your second number : '))
+if choice == 5:
+    number1=float(input('Enter your number : '))
+else:
+    number1=float(input('Enter your first number : '))
+    number2=float(input('Enter your second number : '))
 
 if choice==1:
     print(number1+number2)
@@ -20,5 +26,7 @@ elif choice==2:
     print(number1-number2)
 elif choice==3:
     print(number1*number2)
+elif choice == 5:
+    print(math.sqrt(number1))
 else:
     print(number1/number2)
